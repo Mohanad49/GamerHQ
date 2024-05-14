73f3dd9e47fad560c281843d609e45964a2ece24
Mohanad <mohanned.3262@gmail.com>
2024-05-14 22:51:23 +0300

    Displaying the genres

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,22 @@ import GameGrid from "./components/GameGrid"
 import GenreList from "./components/GenreList"
 
 function App() {
-return <Grid templateAreas={{
-  base: `"nav" "main"`,
-  lg: `"nav nav" "aside main"`
-}}>
+return (
+  <Grid 
+    templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}
+    templateColumns={{
+      base: '1fr',
+      lg: '200px 1fr',
+    }}
+  >
   <GridItem area="nav">
     <NavBar />
   </GridItem>
   <Show above="lg">
-  <GridItem area="aside">
+  <GridItem area="aside" paddingX={5}>
     <GenreList />
   </GridItem>
   </Show>
@@ -20,6 +27,7 @@ return <Grid templateAreas={{
     <GameGrid />
   </GridItem>
 </Grid>
+)
 }
 
 export default App
